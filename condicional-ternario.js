7adf1b5adc810d92642b7a0d3af71a32b50d3abf
omiras <oscar.miras@gmail.com>
2023-10-09 13:28:01 +0200

    ternary

---
+++ condicional-ternario.js
@@ -0,0 +1,19 @@
+/**
+ * En ocasiones, es más cómo escribir una condición boleana en una sola línea, y asignar un valor u otro a la variable
+ */
+
+let nota = 0;
+let calificacion;
+console.log("He realizado mi examen. Mi resultado es el siguiente:");
+
+// if (nota < 5) {
+//   // Acción A: nota es menor que 5
+//   calificacion = "suspendido";
+// } else {
+//   // Acción B: Cualquier otro caso diferente a A (nota es mayor o igual que 5)
+//   calificacion = "aprobado";
+// }
+
+calificacion = nota < 5 ? "suspendido" : "aprobado";
+
+console.log("Estoy", calificacion);
