b8b7d6ced482a08e7089641898552aef2f4e4923
omiras <oscar.miras@gmail.com>
2023-10-09 13:31:34 +0200

    updated

--- condicional-ternario.js
+++ condicional-ternario.js
@@ -17,3 +17,7 @@ console.log("He realizado mi examen. Mi resultado es el siguiente:");
 calificacion = nota < 5 ? "suspendido" : "aprobado";
 
 console.log("Estoy", calificacion);
+
+// variable boleana que me indica si estoy aprobado o suspendido
+let estoyAprobado = nota >= 5;
+console.log("Estoy aprobado? ", estoyAprobado);
