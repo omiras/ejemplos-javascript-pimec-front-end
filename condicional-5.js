a35af59e9ab87f78cb2f814c8a2a1f7916be0f82
omiras <oscar.miras@gmail.com>
2023-10-09 13:06:44 +0200

    updated

---
+++ condicional-5.js
@@ -0,0 +1,14 @@
+/**
+ * Operador ! : Operador de negación . El que fa, es negar una expresió boleana.
+ * Canvia el valor true a false, y viceversa
+ */
+
+/**
+ * Tengo una app Cofidis para dar un credito a alguien que me lo pida, si NO está en la lista de morosos
+ */
+
+let esUsuarioMoroso = true;
+
+if (!esUsuarioMoroso) {
+  console.log("Le concedemos el crédito");
+}
---
+++ strings-1.js
@@ -0,0 +1,12 @@
+/**
+ * Strings son un tipo de dato iterable
+ * Ultimo ejercicio de variables del 5 de octubre hay ejemplos
+ */
+
+let lenguaje = "JavaScript";
+console.log(
+  "quiero acceder a la primera letra del string almacenado en la variable lenguaje: ",
+  lenguaje[0]
+);
+
+console.log("Quedarme con un trocito del string: ", lenguaje.substring(0, 4));
