dd63451c82199922e3b31f3f8151d7576db66409
omiras <oscar.miras@gmail.com>
2023-10-10 10:35:30 +0200

    recorrer string

---
+++ bucle-while.js
@@ -0,0 +1,11 @@
+/**
+ * Caso de uso: Ejecutar un bloque de código mientras se cumpla cierta condición
+ *
+ */
+
+let i = 0;
+
+while (i < 10) {
+  console.log("Pintar circulo");
+  i++;
+}
--- strings-1.js
+++ strings-1.js
@@ -10,3 +10,9 @@ console.log(
 );
 
 console.log("Quedarme con un trocito del string: ", lenguaje.substring(0, 4));
+
+// Recorrer un string porque es un dato iterable
+lenguaje = "JavaScript";
+for (let i = 0; i < lenguaje.length; i++) {
+  console.log(lenguaje[i]);
+}
