e5508ce43bc56a8a187513efd23e1adaf6ca52eb
omiras <oscar.miras@gmail.com>
2023-10-23 09:23:22 +0200

    update

---
+++ algoritmo-57-numero-consecutivo.js
@@ -0,0 +1,22 @@
+function firstNonConsecutive(arr) {
+  let i = 1; //Inicio de la variable del contador
+  let result = null;
+
+  while (i < arr.length && result == null) {
+    // mentre no hagi arribar al fi de l'array I no hagi trobat un número no consecutiu, tinc feina
+    //Recorremos el array de  números mientras se cumpla la condición
+    console.log("Voy a comprobar el número: ", arr[i]);
+
+    if (arr[i] - arr[i - 1] !== 1) {
+      //Comprobamos si el número actual y el número anterior no se incrementan en 1
+      result = arr[i]; //Si se cumple, mostramos el número
+    }
+    i++; //Si el siguiente número se incrementa en 1, incrementamos el valor de la i, y volemos a iniciar el bucle
+  }
+
+  return result; //Si al recorrer todo el array, se cumple mostramos null
+}
+
+console.log(firstNonConsecutive([1, 2, 3, 4, 6, 7, 8])); // 6
+// console.log(firstNonConsecutive([1, 2, 3, 4])); //  null (porque todos son consecutivos)
+// console.log(firstNonConsecutive([7, 8, 10])); // 10
