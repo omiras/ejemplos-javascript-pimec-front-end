d3734f9ce4a8e2af434b0d0fee13689618a09a69
omiras <oscar.miras@gmail.com>
2023-10-26 10:26:33 +0200

    update

---
+++ metodos-array.js
@@ -0,0 +1,11 @@
+/**
+ * Los métodos de array son unos métodos de los arrays que nos permiten realizar operaciones tediosas de manera más snecilla
+ */
+
+let peces = ["Dory", "Nemo", "Burbujitas"];
+
+function mostrarPezPorConsola(pez) {
+  console.log(pez);
+}
+
+peces.forEach(mostrarPezPorConsola);
