e83c4521c3ead93c9d4aad9c6a9eca9a57c4e905
omiras <oscar.miras@gmail.com>
2025-03-11 19:18:27 +0100

    loops

--- bucles/bucle-while.js
+++ bucles/bucle-while.js
@@ -1,11 +1,30 @@
 /**
- * Caso de uso: Ejecutar un bloque de código mientras se cumpla cierta condición
- *
+ * Quiero printar 2 veces por pantalla un texto
  */
 
+// variable de iteración. Te la tienes que crear y gestionarla tú
 let i = 0;
 
-while (i < 10) {
-  console.log("Pintar circulo");
-  i++;
+while (i < 2) {
+  console.log(`Me encuentra en la iteración ${i + 1}`)
+  console.log("Hola! Me ejecuto");
+  console.log("Yo también!");
+  console.log(`Fin de  la iteración ${i + 1}`)
+
+  // incrementar la variable de iteración 
+  i = i + 1;
 }
+
+/**
+ * Para entrar en el curso de IronHack tenemos que cumplir con 10 condiciones
+ * 
+ * 1. Usamos un while para ir comprobando una a una cada una de estas condiciones
+ *   1.2 - residir españa
+ *   1.3 - estar trabajando
+ *   1.4 ....
+ *   1.30.....
+ * 
+ * while (i<numCondiciones && estasTrabajando) {
+ * }
+ */
+
