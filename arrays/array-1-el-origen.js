a37c8ee341f5d176ec8ba01d4ecda0238b06d0ae
omiras <oscar.miras@gmail.com>
2025-03-17 18:09:55 +0100

    done

---
+++ arrays/array-1-el-origen.js
@@ -0,0 +1,9 @@
+/**
+ * Nos piden implementar un programa que nos permita almacenar las retiradas de dinero e ingresos de tu cuenta bancaria
+ */
+
+let saldo = 650000;
+let retirada1 = 20;
+let ingresos1 = 1500;
+let retirada2 = 45;
+let retirada3 = 30;
