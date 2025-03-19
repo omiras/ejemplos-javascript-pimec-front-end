2790b8b8a05ae025b002ef6f8c14a5211b2b8fce
omiras <oscar.miras@gmail.com>
2025-03-19 19:28:17 +0100

    done

--- objetos/objetos-1.js
+++ objetos/objetos-1.js
@@ -1,5 +1,5 @@
 /**
- * En un formulario de registro, me gustaría almacenar la información de reservar un escape room en escaperrombarcelona.com
+ * En un formulario de registro, me gustaría almacenar la información de reservar un escape room en escaperoombarcelona.com
  *
  * - dia
  * - hora
@@ -31,37 +31,42 @@ let registro = {
   },
 };
 
+// Ey! Que al final no vamos a ser 10 participantes..vamos a ser 9 porque alguien se ha dado de baja
+registro.numParticipantes = 9;
+console.log(registro);
+
+
 // Dime si este escape tiene la característica "Fantasia". Dame un true si la tiene, o un false si no la tiene
-console.log(registro.caracteristicas.includes("Fantasia"));
+console.log("¿Este escape room tiene una característica de Fantasía?: ", registro.caracteristicas.includes("Fantasia"));
 
-// ¿Cómo se llama el escape que hemos reservado?
-console.log(registro.nomEscape);
+// // ¿Cómo se llama el escape que hemos reservado?
+console.log("¿Cómo se llama el room escape?: ", registro.nomEscape);
 
-// Dime una carácteristica de este escape, la primera
-console.log(registro.caracteristicas[0]);
+// // Dime una carácteristica de este escape, la primera
+// console.log(registro.caracteristicas[0]);
 
-// Llamamos nosotros y decimos que el 30 de Octubre no va bien porque no puede venir Galis. 01-11-2023
-// Para actualizar un objeto:
+// // Llamamos nosotros y decimos que el 30 de Octubre no va bien porque no puede venir Galis. 01-11-2023
+// // Para actualizar un objeto:
 
-registro.dia = "01-11-2023";
-console.log(registro);
+// registro.dia = "01-11-2023";
+// console.log(registro);
 
-// Despues de muchos de escapes, nos han dicho que realmente este escape no es tan oscuro. Elimina esa caracteristica del escape
-registro.caracteristicas.pop();
-console.log(registro);
+// // Despues de muchos de escapes, nos han dicho que realmente este escape no es tan oscuro. Elimina esa caracteristica del escape
+// registro.caracteristicas.pop();
+// console.log(registro);
 
-// Al final nos dejan que participe una persona más, un total de 11
-// ¿Cómo actualizamos numParticipates?
-registro.numParticipantes = 11;
-console.log(registro);
+// // Al final nos dejan que participe una persona más, un total de 11
+// // ¿Cómo actualizamos numParticipates?
+// registro.numParticipantes = 11;
+// console.log(registro);
 
-// Añadir comentarios al registro en tiempo de ejecución
-registro.comentarios = "Miedo a las arañas";
-console.log(registro);
+// // Añadir comentarios al registro en tiempo de ejecución
+// registro.comentarios = "Miedo a las arañas";
+// console.log(registro);
 
-// borrar propiedad
-delete registro.hora;
-console.log(registro);
+// // borrar propiedad
+// delete registro.hora;
+// console.log(registro);
 
-// Si quiero acceder a la calle donde se encuentra el escape
-console.log(registro.direccion.calle);
+// // Si quiero acceder a la calle donde se encuentra el escape
+// console.log(registro.direccion.calle);
--- objetos/objetos.js
+++ objetos/objetos.js
@@ -22,17 +22,34 @@ const abeja = {
 
 const pato = {
     patas: 2,
-    descripción: "Bípedo aquático",
+    descripcion: "Bípedo aquático",
     rasgos: ["Plumas", "Pico"]
 }
 
 // Operaciones habituales: consultar una propiedad
 
 /* Cuantas patas tiene la abeja ? */
-console.log(pato.patas);
+// console.log(pato.patas);
 
 /* Dime los rasgos de la abeja */
-console.log(abeja.rasgos);
+// console.log(abeja.rasgos);
 
 /* Dime el número de rasgos que tiene la abeja */
-console.log(abeja.rasgos.length);
+// console.log(abeja.rasgos.length);
+
+// Podemos tener arrays de objetos
+const animales = [abeja, pato];
+// console.log("Array de animales: ", animales);
+
+// Itera este array y muestrame por consola la descripción de cada animal. 
+for (let i=0; i<animales.length; i++) {
+    console.log(animales[i].descripcion);
+}
+
+let numPatas = 0;
+// Crear otro bucle para sumar todas las patas de los animales del array de animales
+for (let i=0; i<animales.length; i++) {
+    numPatas = numPatas + animales[i].patas;
+}
+
+console.log("La suma de las patas de todos los animales es: ", numPatas);
